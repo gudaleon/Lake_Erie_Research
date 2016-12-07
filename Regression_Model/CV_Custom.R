8f1127d7646533fcff543572052823866f36380d
Catherine "Catrina" Nowakowski <nowakowskicatherine@gmail.com>
2016-12-07 00:17:11 -0500

    Update CV_Custom.R

--- Regression_Model/CV_Custom.R
+++ Regression_Model/CV_Custom.R
@@ -14,7 +14,8 @@
 ## Plot the n-1 with the omitted 
 
 ## Iniate n and df, df is so I don't mess df_i up
-n = 10
+# if n is a factor of 112 then comment out the two remander sections, if not be sure to include them
+n = 14
 df_i <- as.data.frame(df_i)
 df <- as.data.frame(df_i)
 
@@ -24,6 +25,9 @@ set_name <- matrix(data = NA, nrow = 1, ncol = n)
 regress_name <- matrix(data = NA, nrow = 1, ncol = n)
 prediction_names <- matrix(data = NA, nrow = 1, ncol = n)
 measured_val_names <- matrix(data = NA, nrow = 1, ncol = n)
+RMSE <- matrix(data = NA, nrow = 1, ncol = n)
+Correla <- matrix(data = NA, nrow = 1, ncol = n)
+one_to_one <- matrix(data = NA, nrow = 1, ncol = n)
 
 
 ## Calculates the sample size
@@ -84,14 +88,14 @@ i = i+1
 for(i in 1:n){
   predicted_values <- predict(get(regress_name[i]), get(sample_name[i]) )
   
-  if(i <= n-1){
-  remander <-  (length(df_i$Chlor) - sample_size*(10))  ## not quite sure why I need -1 
-  a <- length(predicted_values)  
-  for(j in 1:remander){
-      predicted_values[(a+j)] <-NA
-    }
-    
-  }
+  # if(i <= n-1){
+  # 
+  # a <- length(predicted_values)
+  # for(j in 1:remander){
+  #     predicted_values[(a+j)] <-NA
+  #   }
+  #   
+  # }
   assign(paste0("prediction_", i), predicted_values)
   prediction_names[i] <- paste0("prediction_", i)
   
@@ -112,14 +116,15 @@ for(i in 1:n){
   a <- get(sample_name[i])
   a <- a$Chlor
   
-  if(i <= n-1){
-    remander <-  (length(df_i$Chlor) - sample_size*(10))  ## not quite sure why I need -1 
-    a_length <- length(a)  
-    for(j in 1:remander){
-      a[(a_length+j)] <-NA
-    }
-    
-  }
+  # if(i <= n-1){
+  #   remander <-  (length(df_i$Chlor) - sample_size*(10))  ## not quite sure why I need -1 
+  # 
+  #   a_length <- length(a)  
+  #   for(j in 1:remander){
+  #     a[(a_length+j)] <-NA
+  #   }
+  #   
+  # }
   
   assign(paste0("Chlor_", i), a)
   measured_val_names[i] <- paste0("Chlor_", i)
@@ -133,21 +138,60 @@ names(Predictions) <- c(prediction_names, measured_val_names)
 
 
 ###################################################################################################
-## Grab vs the predicted
+## Making all of the Plots!!
 # x is sample and Y is predicted
-x <- Predictions$Chlor_2
-y <- Predictions$prediction_2
+pdf("D:/OneDrive/Documents/School-Catrina/Research/Code Post Conferance/Regression Model/CV_Plots.pdf")
 
-RMSE <- round(sqrt( sum( (y - x)^2 , na.rm = TRUE ) / length(y)), 3)
+#########################
+## Grab vs the predicted
+x <-  as.data.frame(df_i$Chlor)
+names(x) <- c("Samples")
 
-Correla <-  round(cor(x, y, use="complete"), 3)
+y <-  as.data.frame(predict(regress_model_final))
+names(y) <- c("Predicted")
+y_no_na <- y[!is.na(y)]
+
+RMSE <- round(sqrt( sum( (y$Predicted - x$Samples)^2 , na.rm = TRUE ) / length(y_no_na)), 3)
+
+Correla <-  round(cor(x$Samples, y$Predicted, use="complete"), 3)
+
+one_to_one <- qplot(x,y) + 
+  geom_abline(intercept = 0, colour = "red", size = 1) + 
+  geom_smooth(method = "lm", se = FALSE) +
+  annotate("text", x = 2.5, y = 0, size = 5,label =paste0("R2: ", Correla )) +
+  annotate("text", x = 2.5, y = .5, size = 5,label =paste0("RMSE: ", RMSE )) +
+  xlab("Sample Chlorophyll") + ylab("Predicted Chlorophyll")
+print(one_to_one)
+
+
+
+for(i in 1:n){
+x <- Predictions[, (n+i)]
+y <- Predictions[, i]
+
+RMSE[i] <- round(sqrt( sum( (y - x)^2 , na.rm = TRUE ) / length(y)), 3)
+
+Correla[i] <-  round(cor(x, y, use="complete"), 3)
 
 one_to_one <- qplot(x,y) +
   geom_abline(intercept = 0, colour = "red", size = 1) +
   geom_smooth(method = "lm", se = FALSE) +
-  annotate("text", x = 1, y = 0, size = 5,label =paste0("R2: ", Correla )) +
-  annotate("text", x = 1, y = .5, size = 5,label =paste0("RMSE: ", RMSE )) +
-  xlab("Sample Chlorophyll") + ylab("Predicted Chlorophyll")
-show(one_to_one)
+  annotate("text", x = 1, y = 0, size = 5,label =paste0("R2: ", Correla[i] )) +
+  annotate("text", x = 1, y = .5, size = 5,label =paste0("RMSE: ", RMSE[i] )) +
+  xlab("Sample Chlorophyll") + ylab("Predicted Chlorophyll") +
+  ggtitle(paste("Group", i))
+print(one_to_one)
+
+}
 
 
+
+## END PDF
+dev.off()
+
+mean_RMSE <- mean(RMSE)
+print("RMSE")
+print(mean_RMSE)
+mean_Correla <- mean(Correla)
+print("R^2")
+print(mean_Correla)
